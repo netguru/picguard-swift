8931ffe4a8e6a653bdfb6447ca4496fa48670e9b
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-04-19 18:59:12 +0200

    Add dummy sources and specs

---
+++ Sources/Dummy.swift
@@ -0,0 +1,10 @@
+//
+// Dummy.swift
+//
+// Copyright (c) 2016 Netguru Sp. z o.o. All rights reserved.
+// Licensed under the MIT License.
+//
+
+public func dummyAnswerToTheUniverse() -> Int {
+	return 42
+}
