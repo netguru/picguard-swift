411fb5c0eb138f76a4316c2ef1d7c484387f29e4
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-04-25 20:38:11 +0200

    Add missing doc comments

--- Sources/Base64ImageEncoder.swift
+++ Sources/Base64ImageEncoder.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
+/// A default base64 encoder to be used with Google Cloud Vision API.
 public struct Base64ImageEncoder: ImageEncoding {
 
+	/// Initializes the instance.
 	public init() {}
 
 	/// Describes errors which may occur during encoding.
@@ -16,6 +18,15 @@ public struct Base64ImageEncoder: ImageEncoding {
 		case UnsupportedBitmapData
 	}
 
+	/// Encodes the image into base64 representation.
+	///
+	/// - Throws: `UnsupportedBitmapData` if the given image contains no bitmap
+	/// data or contains data in unsupported bitmap format.
+	///
+	/// - Parameter image: An image to be encoded.
+	///
+	/// - Returns: A string which contains encoded representation of the given
+	/// image.
 	public func encode(image image: UIImage) throws -> String {
 		guard let data = UIImagePNGRepresentation(image) else {
 			throw Error.UnsupportedBitmapData
--- Sources/ImageEncoding.swift
+++ Sources/ImageEncoding.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
+/// Describes a type that is capable of endoding an image to be used with
+/// Google Cloud Vision API.
 public protocol ImageEncoding {
+
+	/// Encodes an image.
+	///
+	/// - Parameter image: An image to be encoded.
+	///
+	/// - Returns: A string which contains encoded representation of the given
+	/// image.
 	func encode(image image: UIImage) throws -> String
+
 }
