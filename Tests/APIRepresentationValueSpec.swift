69414d738871f9ae4ce8a144c562d3e65a0186f3
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-04-28 13:40:52 +0200

    Reorganize and add complete APIRepresentationValue spec

--- Tests/APIRepresentationValueSpec.swift
+++ Tests/APIRepresentationValueSpec.swift
@@ -15,54 +15,146 @@ final class APIRepresentationValueSpec: QuickSpec {
 
 		describe("APIRepresentationValue") {
 
+			func initWithValueShoudSucceed(value value: AnyObject, expected: APIRepresentationValue) {
+				it("should succeed to initialize") {
+					expect {
+						try APIRepresentationValue(value: value)
+					}.to(equal(expected))
+				}
+			}
+
+			func initWithValueShouldFail(value value: AnyObject) {
+				it("should fail to intialize") {
+					expect {
+						try APIRepresentationValue(value: value)
+					}.to(throwError(APIRepresentationError.UnsupportedInitType))
+				}
+			}
+
+			func initWithDataShouldSucceed(data data: NSData, expected: APIRepresentationValue) {
+				it("should succeed to initialize") {
+					expect {
+						try APIRepresentationValue(data: data)
+					}.to(equal(expected))
+				}
+			}
+			
+			func itShouldFailToInitialize(data data: NSData) {
+				it("should fail to intialize") {
+					expect {
+						try APIRepresentationValue(data: data)
+					}.to(throwError(APIRepresentationError.UnsupportedInitType))
+				}
+			}
+
+			func serializeAPIRepresentation(object: AnyObject) -> NSData {
+				return try! NSJSONSerialization.dataWithJSONObject(object, options: [])
+			}
+
+			func getNonOptionalElementShouldSucceed<T: protocol<Equatable, APIRepresentationConvertible>>(subject subject: APIRepresentationValue, key: String, expected: T) {
+				it("should succeed to get value") {
+					expect { _ -> T in
+						try subject.get(key)
+					}.to(equal(expected))
+				}
+			}
+
+			func getNonOptionalElementShouldFail<T: APIRepresentationConvertible>(subject subject: APIRepresentationValue, key: String, type: T.Type, expectedError: APIRepresentationError) {
+				it("should fail to get value") {
+					expect { _ -> T in
+						try subject.get(key)
+					}.to(throwError(expectedError))
+				}
+			}
+
+			func getOptionalElementShouldSucceed<T: protocol<Equatable, APIRepresentationConvertible>>(subject subject: APIRepresentationValue, key: String, expected: T?) {
+				it("should succeed to get value") {
+					if let expected = expected {
+						expect { _ -> T? in
+							try subject.get(key)
+						}.to(equal(expected))
+					} else {
+						expect { _ -> T? in
+							try subject.get(key)
+						}.to(beNil())
+					}
+				}
+			}
+
+			func getOptionalElementShouldFail<T: APIRepresentationConvertible>(subject subject: APIRepresentationValue, key: String, type: T.Type, expectedError: APIRepresentationError) {
+				it("should fail to get value") {
+					expect { _ -> T? in
+						try subject.get(key)
+					}.to(throwError(expectedError))
+				}
+			}
+
+			func getArrayShouldSucceed<T: protocol<Equatable, APIRepresentationConvertible>>(subject subject: APIRepresentationValue, key: String, expected: [T]) {
+				it("should succeed to get value") {
+					expect { _ -> [T] in
+						try subject.get(key)
+					}.to(equal(expected))
+				}
+			}
+
+			func getArrayShouldFail<T: APIRepresentationConvertible>(subject subject: APIRepresentationValue, key: String, type: T.Type, expectedError: APIRepresentationError) {
+				it("should fail to get value") {
+					expect { _ -> [T] in
+						try subject.get(key)
+					}.to(throwError(expectedError))
+				}
+			}
+
+			func getDictionaryShouldSucceed<T: protocol<Equatable, APIRepresentationConvertible>>(subject subject: APIRepresentationValue, key: String, expected: [String: T]) {
+				it("should succeed to get value") {
+					expect { _ -> [String: T] in
+						try subject.get(key)
+					}.to(equal(expected))
+				}
+			}
+
+			func getDictionaryShouldFail<T: APIRepresentationConvertible>(subject subject: APIRepresentationValue, key: String, type: T.Type, expectedError: APIRepresentationError) {
+				it("should fail to get value") {
+					expect { _ -> [String: T] in
+						try subject.get(key)
+					}.to(throwError(expectedError))
+				}
+			}
+
+			// MARK: -
+
 			describe("init with value") {
 
-				func itShouldSuccessfullyInitialize(value value: AnyObject, expected: APIRepresentationValue) {
-					it("should successfully initialize") {
-						expect {
-							try APIRepresentationValue(value: value)
-						}.to(equal(expected))
-					}
-				}
-
-				func itShouldFailToInitialize(value value: AnyObject) {
-					it("should fail to intialize") {
-						expect {
-							try APIRepresentationValue(value: value)
-						}.to(throwError(APIRepresentationError.UnsupportedInitType))
-					}
-				}
-
 				context("with NSNull value") {
-					itShouldSuccessfullyInitialize(value: NSNull(), expected: .Null)
+					initWithValueShoudSucceed(value: NSNull(), expected: .Null)
 				}
 
 				context("with boolean NSNumber value") {
-					itShouldSuccessfullyInitialize(value: NSNumber(bool: true), expected: .Bool(true))
+					initWithValueShoudSucceed(value: NSNumber(bool: true), expected: .Bool(true))
 				}
 
 				context("with numeric NSNumber value") {
-					itShouldSuccessfullyInitialize(value: NSNumber(long: 123), expected: .Number(123))
+					initWithValueShoudSucceed(value: NSNumber(long: 123), expected: .Number(123))
 				}
 
 				context("with Int value") {
-					itShouldSuccessfullyInitialize(value: 4, expected: .Number(4))
+					initWithValueShoudSucceed(value: 4, expected: .Number(4))
 				}
 
 				context("with Double value") {
-					itShouldSuccessfullyInitialize(value: 5.6, expected: .Number(5.6))
+					initWithValueShoudSucceed(value: 5.6, expected: .Number(5.6))
 				}
 
 				context("with Bool value") {
-					itShouldSuccessfullyInitialize(value: false, expected: .Bool(false))
+					initWithValueShoudSucceed(value: false, expected: .Bool(false))
 				}
 
 				context("with String value") {
-					itShouldSuccessfullyInitialize(value: "foobar", expected: .String("foobar"))
+					initWithValueShoudSucceed(value: "foobar", expected: .String("foobar"))
 				}
 
 				context("with Array value") {
-					itShouldSuccessfullyInitialize(value: [
+					initWithValueShoudSucceed(value: [
 						true,
 						2,
 						"baz"
@@ -74,7 +166,7 @@ final class APIRepresentationValueSpec: QuickSpec {
 				}
 
 				context("with Dictionary value") {
-					itShouldSuccessfullyInitialize(value: [
+					initWithValueShoudSucceed(value: [
 						"foo": false,
 						"bar": 3,
 						"baz": "qux",
@@ -86,35 +178,15 @@ final class APIRepresentationValueSpec: QuickSpec {
 				}
 
 				context("with an invalid value") {
-					itShouldFailToInitialize(value: Set<String>())
+					initWithValueShouldFail(value: Set<String>())
 				}
 
 			}
 
 			describe("init with data") {
 
-				func itShouldSuccessfullyInitialize(data data: NSData, expected: APIRepresentationValue) {
-					it("should successfully initialize") {
-						expect {
-							try APIRepresentationValue(data: data)
-						}.to(equal(expected))
-					}
-				}
-
-				func itShouldFailToInitialize(data data: NSData) {
-					it("should fail to intialize") {
-						expect {
-							try APIRepresentationValue(data: data)
-						}.to(throwError(APIRepresentationError.UnsupportedInitType))
-					}
-				}
-
-				func serializeJSON(object object: AnyObject) -> NSData {
-					return try! NSJSONSerialization.dataWithJSONObject(object, options: [])
-				}
-
 				context("with dictionary of primitives") {
-					itShouldSuccessfullyInitialize(data: serializeJSON(object: [
+					initWithDataShouldSucceed(data: serializeAPIRepresentation([
 						"null": NSNull(),
 						"int": 4,
 						"double": 2.1,
@@ -130,7 +202,7 @@ final class APIRepresentationValueSpec: QuickSpec {
 				}
 
 				context("with array of primitives") {
-					itShouldSuccessfullyInitialize(data: serializeJSON(object: [
+					initWithDataShouldSucceed(data: serializeAPIRepresentation([
 						NSNull(),
 						21,
 						3.7,
@@ -146,7 +218,7 @@ final class APIRepresentationValueSpec: QuickSpec {
 				}
 
 				context("with recursive dictionary") {
-					itShouldSuccessfullyInitialize(data: serializeJSON(object: [
+					initWithDataShouldSucceed(data: serializeAPIRepresentation([
 						"first": [
 							"second": [
 								"third": [
@@ -166,7 +238,7 @@ final class APIRepresentationValueSpec: QuickSpec {
 				}
 
 				context("with recursive array") {
-					itShouldSuccessfullyInitialize(data: serializeJSON(object: [
+					initWithDataShouldSucceed(data: serializeAPIRepresentation([
 						"foo",
 						[
 							"bar",
@@ -183,6 +255,108 @@ final class APIRepresentationValueSpec: QuickSpec {
 							]),
 						]),
 					]))
+				}
+
+			}
+
+			describe("get non optional value") {
+
+				let subject = try! APIRepresentationValue(value: ["first": 123, "second": "foo", "null": NSNull()])
+
+				context("when the key exists and has correct value") {
+					getNonOptionalElementShouldSucceed(subject: subject, key: "first", expected: 123)
+				}
+
+				context("when the key does not exist") {
+					getNonOptionalElementShouldFail(subject: subject, key: "third", type: String.self, expectedError: .MissingDictionaryKey)
+				}
+
+				context("when the key has invalid type") {
+					getNonOptionalElementShouldFail(subject: subject, key: "first", type: String.self, expectedError: .UnexpectedValueType)
+				}
+
+				context("when the subject has icorrect type") {
+					getNonOptionalElementShouldFail(subject: try! APIRepresentationValue(value: "foo"), key: "bar", type: String.self, expectedError: .UnexpectedValueType)
+				}
+
+			}
+
+			describe("get optional value") {
+
+				let subject = try! APIRepresentationValue(value: ["first": 123, "second": "baz", "null": NSNull()])
+
+				context("when the key exists and has correct value") {
+					getOptionalElementShouldSucceed(subject: subject, key: "first", expected: 123)
+				}
+
+				context("when the key does not exist") {
+					getOptionalElementShouldSucceed(subject: subject, key: "third", expected: Optional<String>.None)
+				}
+
+				context("when the key exists and is null") {
+					getOptionalElementShouldSucceed(subject: subject, key: "null", expected: Optional<String>.None)
+				}
+
+				context("when the key has invalid type") {
+					getOptionalElementShouldFail(subject: subject, key: "first", type: String.self, expectedError: .UnexpectedValueType)
+				}
+
+				context("when the subject has icorrect type") {
+					getOptionalElementShouldFail(subject: try! APIRepresentationValue(value: "foo"), key: "bar", type: String.self, expectedError: .UnexpectedValueType)
+				}
+
+			}
+
+			describe("get array") {
+
+				let subject = try! APIRepresentationValue(value: ["first": ["foo", "bar"], "second": ["baz", 123], "null": NSNull()])
+
+				context("when the key exists and holds values of the same type") {
+					getArrayShouldSucceed(subject: subject, key: "first", expected: ["foo", "bar"])
+				}
+
+				pending("when the key exists and holds values of different types") {
+					// not yet supported by the compiler
+					// getArrayShouldSucceed(subject: subject, key: "second", expected: ["baz", 123] as [APIRepresentationConvertible])
+				}
+
+				context("when the key does not exist") {
+					getArrayShouldFail(subject: subject, key: "third", type: String.self, expectedError: .MissingDictionaryKey)
+				}
+
+				context("when the key has invalid type") {
+					getArrayShouldFail(subject: subject, key: "null", type: String.self, expectedError: .UnexpectedValueType)
+				}
+
+				context("when the subject has icorrect type") {
+					getArrayShouldFail(subject: try! APIRepresentationValue(value: "foo"), key: "bar", type: String.self, expectedError: .UnexpectedValueType)
+				}
+
+			}
+
+			describe("get dictionary") {
+
+				let subject = try! APIRepresentationValue(value: ["first": ["foo": 123, "bar": 456], "second": ["foo": 123, "bar": "baz"], "null": NSNull()])
+
+				context("when the key exists and holds values of the same type") {
+					getDictionaryShouldSucceed(subject: subject, key: "first", expected: ["foo": 123, "bar": 456])
+				}
+
+				pending("when the key exists and holds values of different types") {
+					 // not yet supported by the compiler
+					 // getDictionaryShouldSucceed(subject: subject, key: "second", expected: ["foo": 123, "bar": "baz"] as [String: APIRepresentationConvertible])
+				}
+
+				context("when the key does not exist") {
+					getDictionaryShouldFail(subject: subject, key: "third", type: String.self, expectedError: .MissingDictionaryKey)
+				}
+
+				context("when the key has invalid type") {
+					getDictionaryShouldFail(subject: subject, key: "null", type: String.self, expectedError: .UnexpectedValueType)
+				}
+
+				context("when the subject has icorrect type") {
+					getDictionaryShouldFail(subject: try! APIRepresentationValue(value: "foo"), key: "bar", type: String.self, expectedError: .UnexpectedValueType)
 				}
 
 			}
