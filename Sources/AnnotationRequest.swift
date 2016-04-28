791d28b430562ac8066ecdce316e8d6f7e7ca9ab
Lukasz Wolanczyk <lukasz.wolanczyk@netguru.pl>
2016-04-28 10:23:22 +0200

    Added remaining feature types

--- Sources/AnnotationRequest.swift
+++ Sources/AnnotationRequest.swift
@@ -10,12 +10,23 @@ import UIKit
 public struct AnnotationRequest {
 
 	public enum `Type` {
+		case Label(maxResults: Int)
+		case Text(maxResults: Int)
 		case Face(maxResults: Int)
+		case Landmark(maxResults: Int)
+		case Logo(maxResults: Int)
+		case SafeSearch(maxResults: Int)
+		case ImageProperties(maxResults: Int)
+
+		var JSONRepresentation: [String: Any] {
+			return [:]
+		}
 	}
 
 	public enum Image {
 		case URL(String)
 		case Image(UIImage)
+		
 		var JSONRepresentation: [String: Any] {
 			return [:]
 		}
@@ -37,14 +48,26 @@ public struct AnnotationRequest {
 extension AnnotationRequest.`Type`: Hashable {
 	public var hashValue: Int {
 		switch self {
-		case .Face: return 1
+		case .Label: return 1
+		case .Text: return 2
+		case .Face: return 3
+		case .Landmark: return 4
+		case .Logo: return 5
+		case .SafeSearch: return 6
+		case .ImageProperties: return 7
 		}
 	}
 }
 
 public func == (lhs: AnnotationRequest.`Type`, rhs: AnnotationRequest.`Type`) -> Bool {
 	switch (lhs, rhs) {
-	case (.Face, .Face):
-		return true
+	case (.Label, .Label): return true
+	case (.Text, .Text): return true
+	case (.Face, .Face): return true
+	case (.Landmark, .Landmark): return true
+	case (.Logo, .Logo): return true
+	case (.SafeSearch, .SafeSearch): return true
+	case (.ImageProperties, .ImageProperties): return true
+	default: return false
 	}
 }
