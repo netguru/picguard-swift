540c6ea07fc912f9b3ce73071e8f261fcdb40eee
Łukasz Wolańczyk <lukasz.wolanczyk@netguru.pl>
2016-04-29 14:06:28 +0200

    Changes requested in code review

--- Sources/AnnotationRequest.swift
+++ Sources/AnnotationRequest.swift
@@ -51,26 +51,40 @@ public struct AnnotationRequest {
 		var JSONDictionaryRepresentation: [String: AnyObject] {
 			switch self {
 			case .Label(maxResults: let maxResults):
-				return ["type":"LABEL_DETECTION",
-				        "maxResults":maxResults]
+				return [
+					"type":"LABEL_DETECTION",
+					"maxResults":maxResults
+				]
 			case .Text(maxResults: let maxResults):
-				return ["type":"TEXT_DETECTION",
-				        "maxResults":maxResults]
+				return [
+					"type":"TEXT_DETECTION",
+					"maxResults":maxResults
+				]
 			case .Face(maxResults: let maxResults):
-				return ["type":"FACE_DETECTION",
-				        "maxResults":maxResults]
+				return [
+					"type":"FACE_DETECTION",
+					"maxResults":maxResults
+				]
 			case .Landmark(maxResults: let maxResults):
-				return ["type":"LANDMARK_DETECTION",
-				        "maxResults":maxResults]
+				return [
+					"type":"LANDMARK_DETECTION",
+					"maxResults":maxResults
+				]
 			case .Logo(maxResults: let maxResults):
-				return ["type":"LOGO_DETECTION",
-				        "maxResults":maxResults]
+				return [
+					"type":"LOGO_DETECTION",
+					"maxResults":maxResults
+				]
 			case .SafeSearch(maxResults: let maxResults):
-				return ["type":"SAFE_SEARCH_DETECTION",
-				        "maxResults":maxResults]
+				return [
+					"type":"SAFE_SEARCH_DETECTION",
+					"maxResults":maxResults
+				]
 			case .ImageProperties(maxResults: let maxResults):
-				return ["type":"IMAGE_PROPERTIES",
-				        "maxResults":maxResults]
+				return [
+					"type":"IMAGE_PROPERTIES",
+					"maxResults":maxResults
+				]
 			}
 		}
 	}
@@ -96,11 +110,19 @@ public struct AnnotationRequest {
 		func JSONDictionaryRepresentation(encoder: ImageEncoding) throws -> [String: AnyObject] {
 			switch self {
 			case .URL(let URL):
-				return ["source": ["gcs_image_uri": URL]]
+				return [
+					"source": [
+						"gcs_image_uri": URL
+					]
+				]
 			case .Image(let image):
-				return try ["content": encoder.encode(image: image)]
+				return try [
+					"content": encoder.encode(image: image)
+				]
 			case .Data(let data):
-				return try ["content": encoder.encode(imageData: data)]
+				return try [
+					"content": encoder.encode(imageData: data)
+				]
 			}
 		}
 	}
@@ -133,8 +155,10 @@ public struct AnnotationRequest {
 	///
 	/// - Returns: A Dictionary with `String` keys and `AnyObject` values.
 	public func JSONDictionaryRepresentation(encoder: ImageEncoding) throws -> [String: AnyObject] {
-		return ["image": try image.JSONDictionaryRepresentation(encoder),
-		        "features":features.map { $0.JSONDictionaryRepresentation }]
+		return [
+			"image": try image.JSONDictionaryRepresentation(encoder),
+			"features":features.map { $0.JSONDictionaryRepresentation }
+		]
 	}
 }
 
