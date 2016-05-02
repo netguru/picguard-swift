4882f2e39b009567e7a938f5174fc7d8d976b20d
Łukasz Wolańczyk <lukasz.wolanczyk@netguru.pl>
2016-05-02 15:49:00 +0200

    Added AnnotationResponse documentation

--- Sources/AnnotationResponse.swift
+++ Sources/AnnotationResponse.swift
@@ -7,15 +7,34 @@
 
 import Foundation
 
+/// Response containing annotations returned by Google Cloud Vision API.
 public struct AnnotationResponse {
 
+	/// Optional array of `LabelAnnotations`, returns nil if empty
 	public let labelAnnotations: [LabelAnnotation]?
+
+	/// Optional array of `TextAnnotations`, returns nil if empty
 	public let textAnnotations: [Any]?
+
+	/// Optional array of `FaceAnnotations`, returns nil if empty
 	public let faceAnnotations: [Any]?
+
+	/// Optional array of `LandmarkAnnotations`, returns nil if empty
 	public let landmarkAnnotations: [Any]?
+
+	/// Optional array of `LogoAnnotations`, returns nil if empty
 	public let logoAnnotations: [Any]?
+
+	/// Optional `SafeSearchAnnotation`
 	public let safeSearchAnnotation: Any?
+
+	/// Optional `ImagePropertiesAnnotation`
 	public let imagePropertiesAnnotation: Any?
+
+	/// Initializes the receiver with data from response.
+	/// Returns nil if fails to parse response data.
+	///
+	/// - Parameter data: The data returned by Google Cloud Vision API
 
 	public init?(data: NSData) {
 		guard
