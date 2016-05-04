de2c07f3cde8c89f6d30f1aa846d630abd4b1060
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-05-04 21:06:53 +0200

    Break gemoetry file into three files

---
+++ Sources/Position.swift
@@ -0,0 +1,44 @@
+//
+// Position.swift
+//
+// Copyright (c) 2016 Netguru Sp. z o.o. All rights reserved.
+// Licensed under the MIT License.
+//
+
+/// Describes a 3D position inside the image. The position coordinates are in
+/// the same scale as the original image.
+public struct Position: APIRepresentationConvertible {
+
+	/// X coordinate.
+	public let x: Double
+
+	/// Y coordinate.
+	public let y: Double
+
+	/// Z coordinate (or depth).
+	public let z: Double
+
+	// MARK: Initializers
+
+	/// Initializes the receiver.
+	///
+	/// - Parameters:
+	///     - x: X coordinate.
+	///     - y: Y coordinate.
+	///     - z: Z coordinate.
+	public init(x: Double, y: Double, z: Double) {
+		self.x = x
+		self.y = y
+		self.z = z
+	}
+
+	/// - SeeAlso: APIRepresentationConvertible.init(APIRepresentationValue:)
+	public init(APIRepresentationValue value: APIRepresentationValue) throws {
+		try self.init(
+			x: value.get("x"),
+			y: value.get("y"),
+			z: value.get("z")
+		)
+	}
+
+}
