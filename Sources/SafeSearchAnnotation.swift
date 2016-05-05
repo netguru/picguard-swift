d1e4ed6e60e746c56c4b75d4c0ada0979f998606
Łukasz Wolańczyk <lukasz.wolanczyk@netguru.pl>
2016-05-05 15:54:50 +0200

    Started working on SafeSearchAnnotation

---
+++ Sources/SafeSearchAnnotation.swift
@@ -0,0 +1,35 @@
+//
+// LabelAnnotation.swift
+//
+// Copyright (c) 2016 Netguru Sp. z o.o. All rights reserved.
+// Licensed under the MIT License.
+//
+
+public struct SafeSearchAnnotation: APIRepresentationConvertible {
+
+	let adultLikelihood: Likelihood
+	let spoofLikelihood: Likelihood
+	let medicalLikelihood: Likelihood
+	let violenceLikelihood: Likelihood
+
+	public init(
+		adultLikelihood: Likelihood,
+		spoofLikelihood: Likelihood,
+		medicalLikelihood: Likelihood,
+		violenceLikelihood: Likelihood
+	) throws {
+		self.adultLikelihood = adultLikelihood
+		self.spoofLikelihood = spoofLikelihood
+		self.medicalLikelihood = medicalLikelihood
+		self.violenceLikelihood = violenceLikelihood
+	}
+
+	public init(APIRepresentationValue value: APIRepresentationValue) throws {
+		try self.init(
+			adultLikelihood: value.get("adult"),
+			spoofLikelihood: value.get("spoof"),
+			medicalLikelihood: value.get("medical"),
+			violenceLikelihood: value.get("violence")
+		)
+	}
+}
