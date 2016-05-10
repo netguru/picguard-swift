2d02d9804c45275fe473021f6ce76ff69ddae65b
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-05-10 16:39:56 +0200

    Integrate all response fields inside annotation response

--- Sources/AnnotationResponse.swift
+++ Sources/AnnotationResponse.swift
@@ -11,30 +11,58 @@ public struct AnnotationResponse: APIRepresentationConvertible {
 	/// Array of label detection results.
 	public let labelAnnotations: [LabelAnnotation]?
 
-	/// Array of label detection results.
+	/// Array of face detection results.
 	public let faceAnnotations: [FaceAnnotation]?
 
-	/// Array of label detection results.
+	/// Array of text detection results.
 	public let textAnnotations: [TextAnnotation]?
 
-	/// Array of label detection results.
+	/// Array of landmark detection results.
 	public let landmarkAnnotations: [LandmarkAnnotation]?
 
-	/// Array of label detection results.
-	public let safeSearchAnnotations: [SafeSearchAnnotation]?
+	/// Safe search detection result.
+	public let safeSearchAnnotation: SafeSearchAnnotation?
+
+	/// Image properties detection result.
+	public let imagePropertiesAnnotation: ImagePropertiesAnnotation?
 
 	// MARK: Initializers
 
 	/// Initializes the receiver with raw values.
 	///
-	/// - Parameter labelAnnotations: Optional array containing label annotations.
-	public init(labelAnnotations: [LabelAnnotation]?) {
+	/// - Parameters:
+	///     - labelAnnotations: Array containing label annotations.
+	///     - faceAnnotations: Array containing face annotations.
+	///     - textAnnotations: Array containing text annotations.
+	///     - landmarkAnnotations: Array containing landmark annotations.
+	///     - safeSearchAnnotation: Safe search annotation.
+	///     - imagePropertiesAnnotation: Image properties annotation.
+	public init(
+		labelAnnotations: [LabelAnnotation]?,
+		faceAnnotations: [FaceAnnotation]?,
+		textAnnotations: [TextAnnotation]?,
+		landmarkAnnotations: [LandmarkAnnotation]?,
+		safeSearchAnnotation: SafeSearchAnnotation?,
+		imagePropertiesAnnotation: ImagePropertiesAnnotation?
+	) {
 		self.labelAnnotations = labelAnnotations
+		self.faceAnnotations = faceAnnotations
+		self.textAnnotations = textAnnotations
+		self.landmarkAnnotations = landmarkAnnotations
+		self.safeSearchAnnotation = safeSearchAnnotation
+		self.imagePropertiesAnnotation = imagePropertiesAnnotation
 	}
 
 	/// - SeeAlso: APIRepresentationConvertible.init(APIRepresentationValue:)
 	public init(APIRepresentationValue value: APIRepresentationValue) throws {
-		labelAnnotations = try value.get("labelAnnotations")
+		try self.init(
+			labelAnnotations: value.get("labelAnnotations"),
+			faceAnnotations: value.get("faceAnnotations"),
+			textAnnotations: value.get("textAnnotations"),
+			landmarkAnnotations: value.get("landmarkAnnotations"),
+			safeSearchAnnotation: value.get("safeSearchAnnotation"),
+			imagePropertiesAnnotation: value.get("imagePropertiesAnnotation")
+		)
 	}
 }
 
@@ -44,5 +72,12 @@ extension AnnotationResponse: Equatable {}
 
 /// - SeeAlso: Equatable.==
 public func == (lhs: AnnotationResponse, rhs: AnnotationResponse) -> Bool {
-	return lhs.labelAnnotations == rhs.labelAnnotations
+	return (
+		lhs.labelAnnotations == rhs.labelAnnotations &&
+		lhs.faceAnnotations == rhs.faceAnnotations &&
+		lhs.textAnnotations == rhs.textAnnotations &&
+		lhs.landmarkAnnotations == rhs.landmarkAnnotations &&
+		lhs.safeSearchAnnotation == rhs.safeSearchAnnotation &&
+		lhs.imagePropertiesAnnotation == rhs.imagePropertiesAnnotation
+	)
 }
