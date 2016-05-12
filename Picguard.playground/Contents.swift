8ae213fa31b93e7750976791dc510ad7618c5b55
Łukasz Wolańczyk <lukasz.wolanczyk@netguru.pl>
2016-05-12 16:01:50 +0200

    Printing erros in playground

--- Picguard.playground/Contents.swift
+++ Picguard.playground/Contents.swift
@@ -8,15 +8,18 @@ let APIKey = "" // Enter your Google Cloud Vision API key
 let picguard = Picguard(APIKey: APIKey)
 
 picguard.detectUnsafeContent(image: UIImage(named: "dog")!) { result in
-	if case .Success(let unsafeContentLikelihood) = result {
-		switch unsafeContentLikelihood {
-			case .VeryUnlikely: print("It is very unlikely that this image has unsafe content.")
-			case .Unlikely: print("It is unlikely that this image has unsafe content.")
-			case .Possible: print("It is possible that this image has unsafe content.")
-			case .Likely: print("It is likely that this image has unsafe content.")
-			case .VeryLikely: print("It is very likely that this image has unsafe content.")
-			case .Unknown: print("Don't know whether that this image has unsafe content.")
-		}
-		XCPlaygroundPage.currentPage.finishExecution()
+
+	switch result {
+		case .Success(let unsafeContentLikelihood):
+			switch unsafeContentLikelihood {
+				case .VeryUnlikely: print("It is very unlikely that this image has unsafe content.")
+				case .Unlikely: print("It is unlikely that this image has unsafe content.")
+				case .Possible: print("It is possible that this image has unsafe content.")
+				case .Likely: print("It is likely that this image has unsafe content.")
+				case .VeryLikely: print("It is very likely that this image has unsafe content.")
+				case .Unknown: print("Don't know whether that this image has unsafe content.")
+			}
+		case .Error(let error): print("Something went wrong: \(error)")
 	}
+	XCPlaygroundPage.currentPage.finishExecution()
 }
