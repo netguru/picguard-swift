e17e7e56da62a48a8fd6eeb9d68b739b9ab7c30f
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2016-05-20 14:47:42 +0200

    Suppress swiftlint warning

--- Sources/NSImage.swift
+++ Sources/NSImage.swift
@@ -12,7 +12,7 @@ extension NSImage: Base64EncodableImage {
 	public func base64EncodedStringRepresentation() throws -> String {
 		guard
 			let cgImage = self.CGImageForProposedRect(nil, context: nil, hints: nil),
-			let data = NSBitmapImageRep(CGImage: cgImage).representationUsingType(.NSPNGFileType, properties: [:])
+			data = NSBitmapImageRep(CGImage: cgImage).representationUsingType(.NSPNGFileType, properties: [:])
 		else {
 			throw Base64EncodableImageError.UnsupportedBitmapData
 		}
